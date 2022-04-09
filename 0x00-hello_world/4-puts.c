826b6a88ab4d74760c957d8f9dc4c6cc7b208dfd
sun-asefa <snasefa@gmail.com>
2022-04-09 02:18:50 -0700

    my first c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
-/** this is to check puts function
+/**
+ * main - Entry point
  *
- *int main void
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
