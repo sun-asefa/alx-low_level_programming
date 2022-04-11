7e92b2886562cbf0c50926fa5c5147596578886a
sun-asefa <snasefa@gmail.com>
2022-04-11 02:47:05 -0700

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-iwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+pwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 return (1);
 }
