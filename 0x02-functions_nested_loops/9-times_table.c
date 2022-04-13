e09d3ae0b7bab9ba45300b285978ace6c8875594
sun-asefa <snasefa@gmail.com>
2022-04-13 13:40:14 -0700

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * times_table -prints tables
  *
@@ -7,32 +6,42 @@
  */
 void times_table(void)
 {
-int a, b, c;
-
+int a;
+int b;
+int c;
 for (a = 0; a <= 9; a++)
-	{
-		for (b = 0; b <= 9; b++)
-		{
-		c = a * b;
-		if (b == 0)
-		{
-		_putchar(c + '0');
-		}
-		else if (c <= 9)
-		{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(c + '0');
-		}
-		else if (c >= 10)
-		{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(c / 10 + '0');
-		_putchar(c % 10 + '0');
-		}
-	}
-	_putchar('\n');
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+if ((c / 10) == 0)
+{
+if (b == 0)
+{
+_putchar ('0');
+}
+if (b != 0)
+{
+_putchar (' ');
+_putchar ((c % 10) + '0');
+}
+if (b < 9)
+{
+_putchar(',');
+_putchar (' ');
+}
+}
+else
+{
+_putchar ((c / 10) + '0');
+_putchar ((c % 10) + '0');
+if (b < 9)
+{
+_putchar(',');
+_putchar (' ');
+}
+}
+}
+_putchar ('\n');
 }
 }
