c6b6c2132ff489d8b59523002b3bb78a1e3d90db
sun-asefa <snasefa@gmail.com>
2022-04-13 12:01:49 -0700

    time tables

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table -prints tables
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+int a;
+int b;
+int c;
+
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+if (b == 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(c + '0');
+}
+else if (c >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(c / 10 + '0');
+_putchar(c % 10 + '0');
+}
+}
+_putchar('\n');
+}
+}
