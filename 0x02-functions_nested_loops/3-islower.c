1af1c03ed9c92cf7291fb3872e75c5e567ce1402
sun-asefa <snasefa@gmail.com>
2022-04-12 20:35:09 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  *_islower - Entry point
+  *
+  * Return: Always 0
+  *
+  *@c: to be compared
+  */
+int _islower(int c);
+
+{
+if (c > 'a' && c < 'z')
+
+return (1);
+
+else
+
+return (0);
+
+}
