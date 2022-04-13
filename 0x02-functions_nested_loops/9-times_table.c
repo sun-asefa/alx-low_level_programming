1d8c12c41693782ff0d3768b5cce8cd4bea626c9
sun-asefa <snasefa@gmail.com>
2022-04-13 13:15:13 -0700

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,25 +10,29 @@ void times_table(void)
 int a, b, c;
 
 for (a = 0; a <= 9; a++)
-{
-for (b = 0; b <= 9; b++)
-{
-c = a * b;
-if ((c / 10) == 0)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(c + '0');
-}
-else if (c >= 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(c / 10 + '0');
-_putchar(c % 10 + '0');
-}
-}
-_putchar('\n');
+	{
+		for (b = 0; b <= 9; b++)
+		{
+		c = a * b;
+		if (b == 0)
+		{
+		_putchar(c + '0');
+		}
+		else if (c <= 9)
+		{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(c + '0');
+		}
+		else if (c >= 10)
+		{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(c / 10 + '0');
+		_putchar(c % 10 + '0');
+		}
+	}
+	_putchar('\n');
 }
 }
