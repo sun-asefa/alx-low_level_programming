e8268acdd995723aa9de80f0a0620df736b9d2bb
sun-asefa <snasefa@gmail.com>
2022-04-13 00:58:20 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_last_digit - display last digits
+ *
+ *Return: result
+ *@r: value of int
+ */
+int print_last_digit(int r)
+
+{
+int last_digit;
+
+if (r < 0)
+{
+last_digit = (-1 * (r % 10));
+_putchar (last_digit + '0');
+return (last_digit);
+}
+
+else
+{
+last_digit = (r % 10);
+_putchar (last_digit + '0');
+return (last_digit);
+}
+
+}
