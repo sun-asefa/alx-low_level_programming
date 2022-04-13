52e49fa1c2a50f8ed89cab86560c949628d12eae
sun-asefa <snasefa@gmail.com>
2022-04-12 19:35:33 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+void print_alphabet(void);
+{
+char p;
+
+for (p = 'a'; p <= 'z' ; p++)
+
+_putchar(p);
+}
+_putchar('\n');
+return (0);
+}
