5022fe82f71f8f339ba5a1eb76c3e3a70bcebc5b
sun-asefa <snasefa@gmail.com>
2022-04-14 16:22:15 -0700

    debugging

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ * add: addition of int a, int b value
+ */
+
+int add(int a, int b)
+{
+	int a, b;
+
+	return (a + b);
+}
