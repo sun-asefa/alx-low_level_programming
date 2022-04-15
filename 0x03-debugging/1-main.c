5c03b10757b77b36736ef25b5e6f53b3008122a4
sun-asefa <snasefa@gmail.com>
2022-04-15 04:48:05 +0300

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,17 +8,15 @@
 int main(void)
 {
 	int i;
-
 	printf("Infinite loop incoming :(\n");
-
+	
 	i = 0;
-
 	/**
-	  * don’t delete it!
-	  *
-	  */
-
+	*while (i < 10)
+	*{
+	*putchar(i);
+	*}
+	*/
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
-}
+	}
