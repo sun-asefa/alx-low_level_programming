c7cbd09576a3baf0c31e03c2924cf137104507e3
sun-asefa <snasefa@gmail.com>
2022-04-16 20:25:42 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * mul - function to multiply
+ * @a: int value
+ * @b: int value
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+
