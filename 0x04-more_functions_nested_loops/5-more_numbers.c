f6c82d3e0e026286826b12ea1311f800d335c2c3
sun-asefa <snasefa@gmail.com>
2022-04-18 03:32:57 -0700

    print

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,18 +12,18 @@ void more_numbers(void)
 
 	for (c = 0; c < 10; c++)
 	{
-	for (a = 0; a <= 1; a++)
-	{
-	for (b = 0; b <= 9; b++)
-	{
-if (!(a == 1 && b >= 5))
-	{
-	if (a)
-	_putchar(a + '0');
-	_putchar(b + '0');
-	}
-	}
-	}
-	_putchar('\n');
+		for (a = 0; a <= 1; a++)
+		{
+			for (b = 0; b <= 9; b++)
+			{
+				if (!(a == 1 && b >= 5))
+				{
+					if (a)
+						_putchar(a + '0');
+					_putchar(b + '0');
+				}
+			}
+		}
+		_putchar('\n');
 	}
 }
