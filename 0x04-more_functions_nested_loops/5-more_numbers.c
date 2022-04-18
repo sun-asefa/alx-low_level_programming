fe45c5576e42f1d2c02ad3c67063980ecad7874e
sun-asefa <snasefa@gmail.com>
2022-04-17 23:48:39 -0700

    print

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers
+ *
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int a, b, c;
+
+	for (c = 0; c < 10; c++)
+	{
+	for (a = 0; a <= 1; a++)
+	{
+	for (b = 0; b <= 9; b++)
+	{
+if (!(a == 1 && b >= 5))
+	{
+	if (a)
+	_putchar(a + '0');
+	_putchar(b + '0');
+	}
+	}
+	}
+	_putchar('\n');
+	}
+}
