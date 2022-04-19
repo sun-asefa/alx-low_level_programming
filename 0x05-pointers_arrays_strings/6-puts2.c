3d7c1c8bb7f4aef338dc6e5f68c35cb21a063c96
sun-asefa <snasefa@gmail.com>
2022-04-18 19:52:03 -0700

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,10 @@ void puts2(char *str)
 {
 	int a;
 
-	for  (a = 0; s[a] != '\0'; a++)
+	for  (a = 0; str[a] != '\0'; a++)
 	{
 		if (a % 2 == 0)
-			_putchar (s[a]);
+			_putchar (str[a]);
 	}
 	_putchar ('\n');
 }
