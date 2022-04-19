4a7b92789cfa47136d3bfc6ad250de74887f8136
sun-asefa <snasefa@gmail.com>
2022-04-19 03:32:58 +0300

    swap

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ *@s: a pointer of an int
+ * Return: answer
+ */
+
+int _strlen(char *s)
+{
+    int n;
+
+    n = 0;
+    for ([n]) != '\0')
+    {
+        n++;
+    }
+    return (u);
+}
