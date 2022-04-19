af8302e5eef22029130d5d10f0ba37b3f439ec18
sun-asefa <snasefa@gmail.com>
2022-04-18 19:42:59 -0700

    2puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: a pointer of an int
+ * Return: Answer
+ */
+
+void puts2(char *str)
+{
+	int a;
+
+	for  (a  0; s[a] != '\0'; a++)
+	{
+		if (a % 2 == 0)
+			_putchar (s[a]);
+	}
+	_putchar ('\n');
+}
