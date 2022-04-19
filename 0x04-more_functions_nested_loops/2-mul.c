93308456bd95d3772306d8c4bf821e375361d447
sun-asefa <snasefa@gmail.com>
2022-04-19 01:48:03 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+  1 #include "main.h"
+  2 #include <stdio.h>
+  3
+  4 /**
+  5  * mul -Return result of a*b
+  6  * @a: int value
+  7  * @b: int value
+  8  * Return: result
+  9  */
+
+int mul(int a, int b)
+
+{
+
+	 return (a * b);
+}
