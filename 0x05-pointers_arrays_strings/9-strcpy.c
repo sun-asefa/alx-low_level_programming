510e940b2eb2b9021b1484556a3c87d73245b90e
sun-asefa <snasefa@gmail.com>
2022-04-19 05:52:42 -0700

    string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+  * *_strcpy - copies the string pointed to by src
+  * @dest: destination
+  * @src: source
+  * Return: value of destination
+  **/
+char *_strcpy(char *dest, char *src)
+{
+return (strcpy(dest, src));
+}
