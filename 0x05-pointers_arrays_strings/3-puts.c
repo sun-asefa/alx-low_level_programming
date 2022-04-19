d3a8a67a0946e99a7d77faa3f33e8e061c4dc8c1
sun-asefa <snasefa@gmail.com>
2022-04-19 04:00:48 +0300

    string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: 
+ * Return: Answer
+ */
+void _puts(char *str)
+{
+    char *a;
+    int b;
+    
+    a = str;
+    
+    for (b = 0; a(b) b++)
+    {
+        _putchar (a[b]);
+    
+    }
+    _putchar('\n')
+}
