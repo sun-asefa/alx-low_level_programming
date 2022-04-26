89629572091824cc870102be240c253405136b46
sun-asefa <snasefa@gmail.com>
2022-04-25 18:31:39 -0700

    print

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - a function that prints a chessboard
+ * @a: an array input to print
+ * Return: Nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	int a, b;
+
+	for (a = 0; a < 8; a++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[a][b]);
+		}
+		_putchar('\n');
+	}
+}
