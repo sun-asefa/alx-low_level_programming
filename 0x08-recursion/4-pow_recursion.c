615138072bac5e629b7289e81b428792261c1f78
sun-asefa <snasefa@gmail.com>
2022-04-27 19:45:08 -0700

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that returns the natural square root of a number
+ * @x: integer value 
+ * @y: Power value
+ *
+ * Return: On success 1 & the function should return -1 on error
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+if (y == 0)
+return (1);
+return (x * _pow_recursion(x, y - 1));
+}
