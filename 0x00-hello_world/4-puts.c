7a6b656c20184c6b08aa8d64869e7fbc73be4e23
sun-asefa <snasefa@gmail.com>
2022-04-30 10:51:19 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  *main - print the given sentence
+  *
+  *Return: always 0
+  */
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
