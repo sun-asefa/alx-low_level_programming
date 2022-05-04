2a49a95ca8d136fcea345d0c86d6d67de3491e9d
sun-asefa <snasefa@gmail.com>
2022-05-03 23:58:32 -0700

    freed

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,14 +10,14 @@
  */
 void free_grid(int **grid, int height)
 {
-	int i;
+int i;
 
-	if (grid != NULL || height != 0)
-	{
-		for (i = 0; i < height; i++)
-		{
-			free(grid[i]);
-		}
-		free(grid);
-	}
+if (grid != NULL || height != 0)
+{
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
+}
 }
