93396d632946b77282c1ce07881d565ddd5b2d7f
sun-asefa <snasefa@gmail.com>
2022-05-05 03:23:28 -0700

    count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: arg count
+ * @argv: arg value
+ * Return: always 0 success
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+
+printf("%d\n", argc - 1);
+
+return (0);
+}
