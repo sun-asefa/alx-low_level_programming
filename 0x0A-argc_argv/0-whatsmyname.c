88230600f0e58a8904317b7840aea158edef0a73
sun-asefa <snasefa@gmail.com>
2022-05-05 02:38:11 -0700

    argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: arg count
+ * @argv: arg value
+ * Return: always 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+
+printf("%s\n", argv[argc -1]);
+
+return (0);
+}
