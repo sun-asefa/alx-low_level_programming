759ca13d5bc2d75a498aa570eb2504939a97c4ec
sun-asefa <snasefa@gmail.com>
2022-05-06 05:26:32 -0700

    allocates

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - does what malloc does and checks if null
+ * @b: size to alloc for malloc
+ *
+ * Return: void pointer to allocated mem
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+
+	a = malloc(b);
+	if (a == NULL)
+		exit(98);
+	return (a);
+}
