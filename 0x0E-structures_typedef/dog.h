2ef0873ad1b2ea936038f79281bc091be22f727e
sun-asefa <snasefa@gmail.com>
2022-05-09 11:37:44 -0700

    dogs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,15 +9,17 @@
   */
 typedef struct dog
 {
+
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+
+} dog_t;
 
 #endif
 #ifndef _FUNCTIONS_H
 #define _FUNCTIONS_H
-struct dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
