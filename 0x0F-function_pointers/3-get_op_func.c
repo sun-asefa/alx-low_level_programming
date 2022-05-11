fd1b7add0ede028e5a3938efa1c2dcec2ee8c86f
sun-asefa <snasefa@gmail.com>
2022-05-11 13:12:06 -0700

    function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,68 +1,29 @@
 #include "3-calc.h"
+#include <stdlib.h>
 
 /**
-* op_add - Returns the sum of two numbers
-* @a:the first number
-* @b:the second number
+* get_op_func - selects the correct function to perform
+* the operation asked by the user
+* @s: operator passed as argument
 *
-* Return:The sum of a and b
+* Return: A pointer to the function corresponding to
+* the operator given in parameter
 */
 
-int op_add(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
-}
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
 
-/**
-* op_sub - Returns the substraction of two numbers
-* @a:The first number
-* @b:The second number
-*
-* Return: The Difference of a and b
-*/
-
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-
-/**
-* op_mul - Returns the multiplication of two numbers
-* @a:The first number
-* @b: The second number
-*
-* Return: The multiplication of a and b
-*/
-
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-
-
-/**
-* op_div - Returns the division of two numbers.
-* @a: The first number.
-* @b: The second number.
-*
-* Return: The quotient of a and b.
-*/
-
-int op_div(int a, int b)
-{
-	return (a / b);
-}
-
-/**
-* op_mod - Returns the remainder of the division of two numbers.
-* @a: The first number.
-* @b: The second number.
-*
-* Return: The remainder of the division of a by b.
-*/
-
-int op_mod(int a, int b)
-{
-	return (a % b);
+	return (ops[i].f);
 }
