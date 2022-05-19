9674322d50ad14113016614972a601ff07cb203e
sun-asefa <snasefa@gmail.com>
2022-05-19 06:28:28 -0700

    encode

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,21 +8,22 @@
 
 char *rot13(char *s)
 {
-int i = 0, j = 0;
-char string_rot13[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
-char string_alpha[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	int i, j;
 
-while (s[i] != '\0')
-{
-do {
-if (s[i] == string_alpha[j])
-{
-s[i] = string_rot13[j];
-break;
-}
-j++;
-} while (string_alpha[j] != '\0');
-i++;
-}
-return (s);
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
 }
