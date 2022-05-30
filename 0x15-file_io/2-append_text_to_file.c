c71ad44c505a54d0f27eebd4890c7648a8399556
sun-asefa <snasefa@gmail.com>
2022-05-30 11:30:06 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,32 +6,26 @@
  * @text_content: he NULL terminated string to add at the end of the file
  * Return: 1 on success and -1 on failure
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
-	register int _file, _write, text_len = 0;
+	int fd;
+	int i;
 
-	if (!(filename))
-	{
+	if (filename == NULL)
 		return (-1);
-	}
-	_file = open(filename, O_WRONLY, O_APPEND);
-	if (_file == -1)
-	{
+	if (text_content == NULL)
+		return (1);
+	fd = open(filename, O_APPEND | O_WRONLY);
+	if (fd == -1)
 		return (-1);
-	}
-	if (text_content)
+	for (i = 0; text_content[i] != '\0'; i++)
 	{
-		while (text_content[text_len])
+		if (write(fd, &text_content[i], 1) == -1)
 		{
-			text_len++;
-		}
-		_write = write(_file, text_content, text_len);
-		if (_write == -1)
-		{
+			close(fd);
 			return (-1);
 		}
 	}
-	close(_file);
+	close(fd);
 	return (1);
 }
